3e5bfc7191f6a72edc18a2f521e19f546e34e6e5
bastamon <bastamon@sohu.com>
2018-08-31 21:41:11 +0800

    20180831

---
+++ sword_Offer/00_Find/00_Find.cpp
@@ -0,0 +1,69 @@
+﻿//在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+#include <vector>
+#include <iostream>
+using namespace std;
+class Solution 
+{
+public:
+	//方法1
+	//bool Find(int target, vector<vector<int> > array) 
+	//{
+	//	for each(vector<int> rol in array)
+	//		for each(int value in rol)
+	//			if (value == target)
+	//				return true;
+	//	return false;
+	//}
+
+
+	//方法2
+	bool Find(int target, vector<vector<int>> array )
+	{
+		vector<vector<int>>::iterator row;
+		vector<int>::iterator col;
+		for (row = array.begin(); row != array.end(); row++)
+			for (col = (*row).begin(); col != (*row).end(); col++)
+				if (*col == target)
+					return true;
+		return false;
+	}
+
+	//方法3
+	//bool Find(int target, vector<vector<int>> array)
+	//{
+	//	int rows, cols;
+	//	rows = array.size();
+	//	cols = array[0].size();
+	//	for (int i = 0; i<rows; i++)
+	//		for (int j = 0; j<cols; j++)
+	//			if (target == array[i][j])
+	//				return true;
+	//	return false;
+	//}
+
+
+	Solution(vector<vector<int>> array) : arr(array)
+	{	}
+private:
+	vector<vector<int>> arr;
+};
+
+
+
+int main()
+{
+	vector<int> col;
+	vector<vector<int>>array;
+
+	for (int i = 0; i<5; i++)
+	{
+		col.push_back(i);
+		col.push_back(i);
+		array.push_back(col);
+		col.erase(col.begin(), col.end());
+	}
+	Solution A = Solution(array);
+	cout << A.Find(4,array) << endl;
+	system("pause");
+}
+
---
+++ sword_Offer/01string_replace/01string_replace.cpp
@@ -0,0 +1,63 @@
+//请实现一个函数，将一个字符串中的每个空格替换成“ % 20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are % 20Happy。
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	void replaceSpace(char *str, int length) 
+	{
+		int len = length;
+		for (int i = 0; i < length; i++)
+		{
+			if (*(str + i) == ' ')
+			{
+				len += 2;
+				for (int j = len - 1;j-2 > i;j--)
+				{
+					*(str + j) = *(str + j - 2);
+				}
+				*(str + i) = '%';
+				*(str + i + 1) = '2';
+				*(str + i + 2) = '0';
+				length = len;
+			}
+		}
+	}
+
+	//方法2
+	//void replaceSpace(char *str, int length)
+	//{
+	//	for (int i; i < length; i++)
+	//		if (*ptr == ' ')
+	//			*ptr = ' ';
+	//		else
+	//			ptr++;
+	//}
+
+
+
+
+
+	Solution(string s) :str(s)
+	{
+		ptr = &str[0];
+	}
+
+private:
+	string str;	
+	char *ptr = (char*)malloc(sizeof(string)/sizeof(char));
+};
+
+
+
+int main()
+{
+	string str = "We Are Happy.";
+	char *ptr = (char*)malloc(sizeof(str));
+	ptr = &str[0];
+
+	Solution A = Solution("We Are Happy.");
+	A.replaceSpace(ptr,sizeof(str)/sizeof(str[0]));
+}
