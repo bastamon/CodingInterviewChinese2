5598c6d0d0ec87b24b3a1e3e0d73136936f034d8
bastamon <bastamon@sohu.com>
2018-09-03 20:39:07 +0800

    201809032039

--- sword_Offer/08_jumpFloorII/08_jumpFloorII.cpp
+++ sword_Offer/08_jumpFloorII/08_jumpFloorII.cpp
@@ -5,21 +5,38 @@ using namespace std;
 class Solution 
 {
 public:
-	int jumpFloorII(int number) 
+	// pow(2,n-1)
+	// 2^n-1
+	//int jumpFloorII(int number) 
+	//{
+	//	int result = 0;
+	//	if (number == 0)
+	//	{
+	//		return 0;
+	//	}
+	//	else if (number == 1)
+	//	{
+	//		result = 1;
+	//	}
+	//	else 
+	//	{
+	//		result = 2 * jumpFloorII(number - 1);
+	//	}
+	//	return result;
+	//}
+	int jumpFloorII(int number)
 	{
-		int result = 0;
-		if (number == 0)
-		{
-			result = 0;
-		}
-		else if (number == 1)
-		{
-			result = 1;
-		}
-		else 
-		{
-			result = 2 * jumpFloorII(number - 1);
-		}
-		return result;
+		return number == 0 ? 0 : pow(2, number - 1);
 	}
 };
+
+
+
+int main()
+{
+	int a = 4;
+	Solution A;
+	cout << A.jumpFloorII(a) << endl;
+	system("pause");
+	return 0;
+}
