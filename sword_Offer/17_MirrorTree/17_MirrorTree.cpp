4d1169c72e8c6422f0411a2c47106771a5edf6fa
bastamon <bastamon@sohu.com>
2018-09-06 09:48:37 +0800

    start 17_MirrorTree

---
+++ sword_Offer/17_MirrorTree/17_MirrorTree.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+struct TreeNode 
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+		val(x), left(nullptr), right(nullptr) 
+	{
+	}
+}; 
+class Solution 
+{
+public:
+	//操作给定的二叉树，将其变换为源二叉树的镜像
+	void Mirror(TreeNode *pRoot) 
+	{
+
+		;
+	}
+};
